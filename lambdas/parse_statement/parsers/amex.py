633aeb9ff6fd7b94b51336e7b59f6dae37602c0b
Mufaddal Motiwala <mqmotiwala@gmail.com>
2025-06-01 16:08:29 -0700

    update amex parser

--- lambdas/parse_statement/parsers/amex.py
+++ lambdas/parse_statement/parsers/amex.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 def parse(df):
     return pd.DataFrame({
-        "date": pd.to_datetime(df["Date"]),
-        "description": df["Details"],
-        "amount": df["Amount"].astype(float),
-        "raw_vendor_name": df["Details"]
+        "transaction_date": pd.to_datetime(df["transaction_date"]),
+        "description": df["description"],
+        "amount": df["amount"].astype(float) * -1
     })
