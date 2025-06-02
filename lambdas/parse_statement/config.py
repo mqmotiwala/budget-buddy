100aaad22d81a356a1c7621b0cd11131d7f3099f
Mufaddal Motiwala <mqmotiwala@gmail.com>
2025-06-01 23:27:15 -0700

    add support for more issuers

--- lambdas/parse_statement/config.py
+++ lambdas/parse_statement/config.py
@@ -8,10 +8,38 @@ Required keys for each issuer include:
 """
 
 ISSUER_CONFIG = {
+    "amazon": {
+        "DATE_COLUMN": "Transaction Date",
+        "DESCRIPTION_COLUMN": "Description",
+        "AMOUNT_COLUMN": "Amount",
+        "EXPENSES_SIGN": 1
+    },
+
     "amex": {
         "DATE_COLUMN": "Date",
         "DESCRIPTION_COLUMN": "Description",
         "AMOUNT_COLUMN": "Amount",
         "EXPENSES_SIGN": -1  # Amex statements have expenses as positive amounts
+    },
+
+    "chase": {
+        "DATE_COLUMN": "Transaction Date",
+        "DESCRIPTION_COLUMN": "Description",
+        "AMOUNT_COLUMN": "Amount",
+        "EXPENSES_SIGN": 1
+    },
+
+    "chase_debit": {
+        "DATE_COLUMN": "Posting Date",
+        "DESCRIPTION_COLUMN": "Description",
+        "AMOUNT_COLUMN": "Amount",
+        "EXPENSES_SIGN": 1
+    },
+
+    "rbc": {
+        "DATE_COLUMN": "Transaction Date",
+        "DESCRIPTION_COLUMN": "Description",
+        "AMOUNT_COLUMN": "Amount",
+        "EXPENSES_SIGN": 1
     }
 }
