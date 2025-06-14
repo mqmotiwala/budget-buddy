11bee24d4a60a0a9125a879d29313c32276fd403
Mufaddal Motiwala <mqmotiwala@gmail.com>
2025-06-14 00:34:01 -0700

    modify for use in AWS Step Functions

--- lambdas/parse_statement/lambda_function.py
+++ lambdas/parse_statement/lambda_function.py
@@ -3,10 +3,11 @@ Parse and clean financial statement CSV files uploaded to S3.
 This Lambda function is triggered by S3 events when new files are uploaded to the 'statements' folder.
 """
 
+import json
 import boto3
-import pandas as pd
-import os
 import logging
+import traceback
+import pandas as pd
 
 from config import ISSUER_CONFIG
 from parser import parse
@@ -17,55 +18,76 @@ logger.setLevel(logging.INFO)
 
 s3 = boto3.client('s3')
 
+BUCKET = 'aws-budget-buddy'
+
 def lambda_handler(event, context):
     logger.info(f"Lambda triggered with event: ")
-    logger.info(event)
+    logger.info(json.dumps(event))
 
-    for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = unquote_plus(record['s3']['object']['key'])
+    key = unquote_plus(event.get('key'))
+    logger.info(f"Processing file from bucket: {BUCKET}, key: {key}")
 
-        logger.info(f"Processing file from bucket: {bucket}, key: {key}")
+    try:
+        # extract details from key path
+        # expected format: <user>/statements/<issuer>/file.csv
+        parts = key.split("/")
+        user = parts[0]
+        issuer = parts[2]
 
-        try:
-            # extract details from key path
-            # expected format: <user>/statements/<issuer>/file.csv
-            parts = key.split("/")
-            user = parts[0]
-            issuer = parts[2]
-            if not ISSUER_CONFIG.get(issuer):
-                raise ValueError(f"issuer {issuer} in key: {key} is unsupported/unrecognized")
+        if not ISSUER_CONFIG.get(issuer):
+            return {
+                "statusCode": 422,
+                "body": json.dumps({
+                    "error": f"issuer {issuer} in key: {key} is unsupported/unrecognized"
+                })
+            }
 
-            obj = s3.get_object(Bucket=bucket, Key=key)
-            raw = pd.read_csv(obj['Body'])
+        obj = s3.get_object(Bucket=BUCKET, Key=key)
+        raw = pd.read_csv(obj['Body'])
 
-            logger.info(f"Read {len(raw)} rows from raw CSV")
+        logger.info(f"Read {len(raw)} rows from raw CSV")
 
-            clean = parse(raw, ISSUER_CONFIG[issuer])
-            clean["statement_issuer"] = issuer.lower()
+        clean = parse(raw, ISSUER_CONFIG[issuer])
+        clean["statement_issuer"] = issuer.lower()
 
-            if not clean.empty:
-                # set output key based on issuer and date range
-                min_date = clean["transaction_date"].dropna().min().date().strftime("%Y-%m-%d")
-                max_date = clean["transaction_date"].dropna().max().date().strftime("%Y-%m-%d")
-                output_key = f"{user}/cleaned/{issuer.lower()} activity from {min_date} to {max_date}.csv"
-                logger.info(f"Uploading cleaned file to: {output_key}")
-                
-                s3.put_object(
-                    Bucket=bucket,
-                    Key=output_key,
-                    Body=clean.to_csv(index=False).encode('utf-8')
-                )
-
-            else:
-                logger.warning(f"file {key} for issuer {issuer} is empty after parsing. No output generated.")
+        output_key = None
+        if not clean.empty:
+            # set output key based on issuer and date range
+            min_date = clean["transaction_date"].dropna().min().date().strftime("%Y-%m-%d")
+            max_date = clean["transaction_date"].dropna().max().date().strftime("%Y-%m-%d")
+            output_key = f"{user}/cleaned/{issuer.lower()} activity from {min_date} to {max_date}.csv"
+            logger.info(f"Uploading cleaned file to: {output_key}")
             
-            # streamlit checks for this log to confirm execution success
-            logger.info("SUCCESS")
-            
-        except Exception as e:
-            # streamlit checks for this log to confirm execution failure
-            logger.error("FAILURE")
-            logger.exception(f"Unable to process file {key}: {e}")
-            
-            raise
+            s3.put_object(
+                Bucket=BUCKET,
+                Key=output_key,
+                Body=clean.to_csv(index=False).encode('utf-8')
+            )
+
+        else:
+            logger.warning(f"file {key} for issuer {issuer} is empty after parsing. No output generated.")
+        
+        # check_lambda_completed() looks for this log to confirm execution success
+        logger.info("SUCCESS")
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "parsed_statement_key": output_key
+            })
+        }
+
+        
+    except Exception as e:
+        # check_lambda_completed() looks for this log to confirm execution failure
+        logger.error("FAILURE")
+        logger.exception(f"Unable to process file {key}: {e}")
+        
+        tb = traceback.format_exc()
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "error": str(e),
+                "traceback": tb
+            })
+        }
--- lambdas/update_master/lambda_function.py
+++ lambdas/update_master/lambda_function.py
@@ -3,6 +3,7 @@ Lambda function to update master expenses file when new cleaned files are genera
 This function is triggered by S3 events when new cleaned files are uploaded.
 """
 
+import json
 import boto3
 import pandas as pd
 import logging
@@ -23,106 +24,122 @@ DEDUPLICATION_COLS = ["transaction_id"]
 
 def lambda_handler(event, context):
     logger.info("Lambda triggered with event:")
-    logger.info(event)
+    logger.info(json.dumps(event))
 
-    for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = unquote_plus(record['s3']['object']['key'])
+    body = json.loads(event["body"]) if isinstance(event["body"], str) else event["body"]
+    key = unquote_plus(body["parsed_statement_key"])
 
-        logger.info(f"Processing file from bucket: {bucket}, key: {key}")
+    logger.info(f"Processing file from bucket: {BUCKET}, key: {key}")
 
-        # extract details from key path
-        # expected format: <user>/cleaned/file.csv
-        parts = key.split("/")
-        user = parts[0]
+    # extract details from key path
+    # expected format: <user>/cleaned/file.csv
+    parts = key.split("/")
+    user = parts[0]
 
-        MASTER_KEY = f'{user}/categorized_expenses.parquet'
-        BACKUP_FOLDER = f'{user}/backups'
+    MASTER_KEY = f'{user}/categorized_expenses.parquet'
+    BACKUP_FOLDER = f'{user}/backups'
 
+    try:
+        obj = s3.get_object(Bucket=BUCKET, Key=key)
+        new_data = pd.read_csv(
+            obj['Body'],
+            dtype={
+                'description': 'str',
+                'amount': 'float64',
+                'statement_issuer': 'str',
+                'category': 'str',
+                'notes': 'str'
+            },
+            parse_dates=['transaction_date'],
+        )
+
+        if new_data.empty:
+            logger.warning(f"New file {key} is empty. Skipping update.")
+
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"message": "Master file updated successfully"})
+            }
+
+        else:
+            logger.info(f"Read {len(new_data)} rows from new CSV")
+
+        # Load master file (if it exists)
         try:
-            obj = s3.get_object(Bucket=bucket, Key=key)
-            new_data = pd.read_csv(
-                obj['Body'],
-                dtype={
-                    'description': 'str',
-                    'amount': 'float64',
-                    'statement_issuer': 'str',
-                    'category': 'str',
-                    'notes': 'str'
-                },
-                parse_dates=['transaction_date'],
-            )
+            master_obj = s3.get_object(Bucket=BUCKET, Key=MASTER_KEY)
+            master_body = BytesIO(master_obj['Body'].read())
+            master_df = pd.read_parquet(master_body)
 
-            if new_data.empty:
-                logger.warning(f"New file {key} is empty. Skipping update.")
-                continue
-            else:
-                logger.info(f"Read {len(new_data)} rows from new CSV")
+            num_rows = len(master_df)
+            num_uncategorized = master_df['category'].isna().sum()
+            logger.info(f"Read {num_rows} rows from master file")
 
-            # Load master file (if it exists)
-            try:
-                master_obj = s3.get_object(Bucket=BUCKET, Key=MASTER_KEY)
-                master_body = BytesIO(master_obj['Body'].read())
-                master_df = pd.read_parquet(master_body)
-
-                num_rows = len(master_df)
-                num_uncategorized = master_df['category'].isna().sum()
-                logger.info(f"Read {num_rows} rows from master file")
-
-                # Backup current copy of master file
-                backup_key = f"{BACKUP_FOLDER}/{num_uncategorized}-{num_rows}__{datetime.now().strftime("%Y-%m-%dT%H-%M-%S-%f")}.parquet"
-                s3.copy_object(
-                    Bucket=BUCKET,
-                    CopySource={'Bucket': BUCKET, 'Key': MASTER_KEY},
-                    Key=backup_key
-                )
-
-                logger.info(f"backed up latest master file at {backup_key}")
-
-            except ClientError as e:
-                if e.response['Error']['Code'] == 'NoSuchKey':
-                    logger.warning("Master file not found. Starting fresh.")
-                    master_df = pd.DataFrame(columns=new_data.columns)
-                else:
-                    # streamlit checks for this log to confirm execution failure
-                    logger.error("FAILURE")
-                    logger.exception(f"Unable to process file {key}: {e}")
-                    
-                    raise
-
-            # include required columns in master schema
-            # handles cases where master is empty or missing 
-            for col in REQUIRED_COLS:
-                if col not in master_df.columns:
-                    master_df[col] = pd.NA
-
-            # Merge and deduplicate
-            # we ignore columns that are not in both dataframes, handling categorized expenses gracefully
-            combined = pd.concat([master_df, new_data], ignore_index=True)
-            before = len(combined)
-            combined = combined.drop_duplicates(subset=DEDUPLICATION_COLS)
-            after = len(combined)
-            logger.info(f"Dropped {before - after} duplicate rows. Final row count: {after}")
-
-            # Save to Parquet in memory
-            out_buffer = BytesIO()
-            combined.to_parquet(out_buffer, index=False, compression='snappy')
-
-            # Upload updated master file
-            s3.put_object(
+            # Backup current copy of master file
+            backup_key = f"{BACKUP_FOLDER}/{num_uncategorized}-{num_rows}__{datetime.now().strftime("%Y-%m-%dT%H-%M-%S-%f")}.parquet"
+            s3.copy_object(
                 Bucket=BUCKET,
-                Key=MASTER_KEY,
-                Body=out_buffer.getvalue()
+                CopySource={'Bucket': BUCKET, 'Key': MASTER_KEY},
+                Key=backup_key
             )
 
-            logger.info(f"updated master file at {MASTER_KEY}")
+            logger.info(f"backed up latest master file at {backup_key}")
 
-            # streamlit checks for this log to confirm execution success
-            logger.info("SUCCESS")
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'NoSuchKey':
+                logger.warning("Master file not found. Starting fresh.")
+                master_df = pd.DataFrame(columns=new_data.columns)
+            else:
+                # streamlit checks for this log to confirm execution failure
+                logger.error("FAILURE")
+                logger.exception(f"Unable to process file {key}: {e}")
+                
+                return {
+                    "statusCode": 400,
+                    "body": json.dumps({"error": str(e)})
+                }
 
-        except Exception as e:
-            # streamlit checks for this log to confirm execution failure
-            logger.error("FAILURE")
-            logger.exception(f"Unable to process file {key}: {e}")
+        # include required columns in master schema
+        # handles cases where master is empty or missing 
+        for col in REQUIRED_COLS:
+            if col not in master_df.columns:
+                master_df[col] = pd.NA
 
-            raise
+        # Merge and deduplicate
+        # we ignore columns that are not in both dataframes, handling categorized expenses gracefully
+        combined = pd.concat([master_df, new_data], ignore_index=True)
+        before = len(combined)
+        combined = combined.drop_duplicates(subset=DEDUPLICATION_COLS)
+        after = len(combined)
+        logger.info(f"Dropped {before - after} duplicate rows. Final row count: {after}")
+
+        # Save to Parquet in memory
+        out_buffer = BytesIO()
+        combined.to_parquet(out_buffer, index=False, compression='snappy')
+
+        # Upload updated master file
+        s3.put_object(
+            Bucket=BUCKET,
+            Key=MASTER_KEY,
+            Body=out_buffer.getvalue()
+        )
+
+        logger.info(f"updated master file at {MASTER_KEY}")
+
+        # check_lambda_completed() looks for this log to confirm execution success
+        logger.info("SUCCESS")
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "Master file updated successfully"})
+        }
+
+    except Exception as e:
+        # check_lambda_completed() looks for this log to confirm execution failure
+        logger.error("FAILURE")
+        logger.exception(f"Unable to process file {key}: {e}")
+
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": str(e)})
+        }
+
