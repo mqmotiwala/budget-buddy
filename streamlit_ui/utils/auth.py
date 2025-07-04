dd5a42dbf0fcbeb7ecdcb5bf417e2f4d2f0911c8
mqmotiwala <86031412+mqmotiwala@users.noreply.github.com>
2025-07-04 00:09:53 -0700

    Update auth.py

--- streamlit_ui/utils/auth.py
+++ streamlit_ui/utils/auth.py
@@ -74,7 +74,6 @@ def get_auth(unique_key=None):
 def logout():
     # effectively resets the session
     st.session_state.clear()
-    st.write("kik")
 
     # rerun the app to reflect the new state
     st.rerun()
